1731e5a5586cd1281796c95a93f8169c6c335088
manjula892 <manjulamanjula824892@gmail.com>
2026-04-14 06:29:08 -0700

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+# Arithmetic
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+# Comparison
+print("Is a greater than b?", a > b)
+print("Is a equal to b?", a == b)
