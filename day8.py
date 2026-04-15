31b6b3d1f85553e367fb027908a57094ac2e8dac
manjula892 <manjulamanjula824892@gmail.com>
2026-04-15 03:18:49 -0700

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,9 @@
+def greet(name):
+    print("Hello", name)
+# Calling function
+greet("Manju")
+# Function with return value
+def add(a, b):
+    return a + b
+result = add(5, 3)
+print("Sum:", result)
