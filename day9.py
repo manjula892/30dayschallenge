e0c2cc00b24e6defb405c899b2323499e2e12b70
manjula892 <manjulamanjula824892@gmail.com>
2026-04-18 06:36:38 -0700

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "mango"]
+# Accessing elements
+print("First fruit:", fruits[0])
+# Adding element
+fruits.append("orange")
+# Looping through list
+print("Fruit list:")
+for fruit in fruits:
+    print(fruit)
