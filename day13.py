82252b650b9edcff1a432f0941e178fd098c3d0f
manjula892 <manjulamanjula824892@gmail.com>
2026-04-22 03:15:50 -0700

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,12 @@
+# String Example
+text = "hello manju"
+# Convert to uppercase
+print(text.upper())
+# Convert to lowercase
+print(text.lower())
+# Length of string
+print("Length:", len(text))
+# Replace word
+print(text.replace("manju", "python"))
+# Slicing
+print("First 5 letters:", text[:5])
