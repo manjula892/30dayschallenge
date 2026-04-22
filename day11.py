f0e3e3db3b2ee6def4ae82a7b10470cb809bc5b9
manjula892 <manjulamanjula824892@gmail.com>
2026-04-22 03:14:57 -0700

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,9 @@
+# Set Example
+numbers = {1, 2, 3, 4, 4, 5}
+print("Original set:", numbers)
+# Add element
+numbers.add(6)
+# Loop through set
+print("Updated set:")
+for num in numbers:
+    print(num)
