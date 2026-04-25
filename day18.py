2d3f93b9ff92aa3a10024b827091aff74e029d44
manjula892 <manjulamanjula824892@gmail.com>
2026-04-25 06:50:25 -0700

    Add files via upload

---
+++ day18.py
@@ -0,0 +1,12 @@
+# OOP Example
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+# Creating object
+s1 = Student("Manju", 18)
+# Calling method
+s1.display()
